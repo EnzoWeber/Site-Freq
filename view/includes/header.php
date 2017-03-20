2b44c861c012e8a4ef6804268cc723b9fc6b622e
EnzoWeber <snowfresco@gmail.com>
2017-03-20 22:54:35 +0100

    Header FIX

--- view/includes/header.php
+++ view/includes/header.php
@@ -13,10 +13,10 @@
     <link href="css/styles.css" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    body { padding-top: 100px; }
+
 </head>
 <body>
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+<div class="navbar navbar-default navbar-static-top" role="navigation">
     <div class="container">
         <div class="navbar-header page-scroll">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
